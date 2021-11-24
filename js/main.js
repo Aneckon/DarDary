413c420527f1d5a5b02f42eadd36c80d454286d4
Aneckon <83111616+Aneckon@users.noreply.github.com>
2021-11-24 13:15:31 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,3 +9,14 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
   },
 });
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 3,
+  spaceBetween: 40,     
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
